05ea6c1e5a6fc853e7a7c2b040a5cb855c7a00aa
Arian Treffer <derari@web.de>
2017-05-16 13:57:11 +0200

    Update check.sh
    updated repository url

--- travis/check.sh
+++ travis/check.sh
@@ -2,6 +2,6 @@
 git config --global user.email "profci@example.com"
 git config --global user.name "profci"
 git checkout -b travis-tmp
-git pull -X theirs --no-edit "https://github.com/derari/profci-tasks.git" master
+git pull -X theirs --no-edit "https://github.com/hpi-epic/profci-tasks.git" master
 bundle exec rspec spec/_tdd_tasks/ --fail-fast --format j --out report.txt
 cat report.txt | python travis/submit_issue.py
